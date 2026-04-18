b204c66f8c710ce99f7a2a7224403af2022c2303
sarohi0522 <sarohi@icloud.com>
2026-04-18 13:49:36 -0700

    Add files via upload

---
+++ congragulation message.py
@@ -0,0 +1,8 @@
+name = "Raj"
+score = 95.5
+passed = True
+
+print("Congratulations", name + "!")
+print("Your score is", score)
+print("Passed:", passed)
+print("Great job on completing your project!")
