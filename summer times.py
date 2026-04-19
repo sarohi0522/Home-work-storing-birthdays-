3c5fc682e6bea055a9e21d7e5dd09d6e6956e16e
sarohi0522 <sarohi@icloud.com>
2026-04-19 09:35:12 -0700

    Add files via upload

---
+++ summer times.py
@@ -0,0 +1,8 @@
+temperature = int(input("enter temperature:"))
+
+if temperature > 80:
+    print(" It is really hot outside, stay inside and stay hydrated!")
+elif temperature > 60:
+    print(" It is warm outside, enjoy the weather!")
+else:
+    print(" It is cool outside, bring a light jacket!")
